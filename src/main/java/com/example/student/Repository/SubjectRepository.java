933c6c64511e621cf738424ba31c2d2dcbefe607
Sanjay N <sanjayyaksha3009@gmail.com>
2024-10-14 16:12:07 +0530

    deleting subject by name

--- src/main/java/com/example/student/Repository/SubjectRepository.java
+++ src/main/java/com/example/student/Repository/SubjectRepository.java
@@ -3,8 +3,10 @@ package com.example.student.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.student.entity.Subject;
 
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
+import java.util.Optional;
 
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findByName(String name);
 
 
 }
